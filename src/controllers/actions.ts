ad578fda9b57e55569bf80250c7b0c8121e018da
David Martínez Tapia <dmmtapia.ux@gmail.com>
2024-06-05 20:05:29 -0400

    actions

--- src/controllers/actions.ts
+++ src/controllers/actions.ts
@@ -1,21 +1,12 @@
-import { UserModel } from "../db/userSchema";
+import {UserModel} from "../db/userSchema"
 
-export const getUser = () => UserModel.find();
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email:string) => UserModel.findOne({email});
+export const getUserBySessionToken = (sessionToken:string) => UserModel.findOne({
+    "authentication.sessionToken": sessionToken
+});
 
-export const getUserByEmail = (email: string) => UserModel.findOne({ email });
-
-export const getUserBySessionToken = (sessionToken: string) =>
-  UserModel.findOne({
-    "authentication.sessionToken": sessionToken,
-  });
-
-export const getUserById = (id: string) => UserModel.findById(id);
-
-export const createUser = (values: Record<string, any>) =>
-  new UserModel(values).save().then((user) => user.toObject());
-
-export const updateUser = (id: string, values: Record<string, any>) =>
-  UserModel.findByIdAndUpdate({ values, id });
-
-export const deleteUser = (id: string) =>
-  UserModel.findOneAndDelete({ _id: id });
+export const getUserById = (id:string) => UserModel.findById(id);
+export const createUser = (values:Record<string,any>) => new UserModel(values).save().then((user) => user.toObject());
+export const updateUser = (id:string, values:Record<string,any>)=> UserModel.findByIdAndUpdate(id, values);
+export const deleteUserById = (id:string) => UserModel.findOneAndDelete({_id:id});
