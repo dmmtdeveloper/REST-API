c3daafa622621b3184444ed60cf98048e9d17ec3
David Martínez Tapia <dmmtapia.ux@gmail.com>
2024-06-05 20:07:19 -0400

    users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,16 +1,51 @@
-import express from "express"
-import { getUser } from "../controllers/actions"
+import express from "express";
+import { deleteUserById, getUserById, getUsers } from "./actions";
 
 export const getAllUsers = async(req:express.Request, res:express.Response) => {
     try {
-        const users = await getUser();
-        if(!users){
-            return res.sendStatus(400);
-        };
+        const users = await getUsers();
+        if(!getAllUsers){
+            return res.sendStatus(400)
+        }
+        return res.status(200).json(users)
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
 
-        res.status(200).json(users).end();
+export const deleteUser = async(req:express.Request, res:express.Response) => {
+    try {
+        const {id} = req.params;
+
+        const deletedUser = await deleteUserById(id);
+        return res.json(deletedUser);
+        
     } catch (error) {
         console.log(error)
+        return res.sendStatus(400)
+    }
+}
+
+
+export const updateUser = async(req:express.Request, res:express.Response) => {
+    try {
+        const {username} = req.body;
+        const {id} = req.params;
+
+        if(!username){
+            return res.sendStatus(403)
+        }
+
+        const user = await getUserById(id)
+
+        user.username = username;
+        await user.save();
+
+        return res.status(200).json(user).end();
+
+    } catch (error) {
+        console.log(error);
         return res.sendStatus(400)
     }
 }
