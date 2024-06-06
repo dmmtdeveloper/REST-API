0143bba8e3666cac7168dca35131d4e31600226b
David Martínez Tapia <dmmtapia.ux@gmail.com>
2024-06-05 20:11:55 -0400

    authentication

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -1,8 +1,7 @@
-import express from "express"
-import {register} from "../controllers/register"
-import { login } from "../controllers/login"
+import express from "express";
+import { register, login } from "../controllers/authentication";
 
 export default (router: express.Router) => {
-    router.post("/auth/register", register)
-    router.post("/auth/login", login)
-}
+  router.post("/auth/register", register);
+  router.post("/auth/login", login);
+};
